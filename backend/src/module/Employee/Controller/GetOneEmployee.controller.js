0e56197a46cf6e11128253187c3781a647bdbabc
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-06-03 02:50:28 +0300

    add getOneEmployeeFunc

--- backend/src/module/Employee/Controller/GetOneEmployee.controller.js
+++ backend/src/module/Employee/Controller/GetOneEmployee.controller.js
@@ -1,9 +1,23 @@
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 import  employeeModel from '../../../../DB/models/Employee.model.js';
 
+import mongoose from "mongoose";
 
 export const getOneEmployee = asyncHandler(async (req, res) => {
     const { id } = req.params;
-    const employee = await employeeModel.findById(id);
-    res.status(200).json(employee);
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ success: false, message: "Invalid employee ID format" });
+    }
+
+    const employee = await employeeModel.findById(id).lean();
+
+    if (!employee) {
+        return res.status(404).json({ success: false, message: "Employee not found" });
+    }
+
+    res.status(200).json({
+        success: true,
+        data: employee,
+    });
 });
