c24a0c822b869b8578e9423a25b44f1090ad42a8
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-06-03 05:02:09 +0300

    update models & getallemployess & getOneEployee

--- backend/src/module/Employee/Employee.router.js
+++ backend/src/module/Employee/Employee.router.js
@@ -19,12 +19,14 @@ router
   .route("/")
   .post(validation(addEmployeeSchema), addEmployee)
   .get(getAllEmployee);
-  router
+router
   .route("/:id")
   .put(validation(updateEmployeeSchema), updateEmployee)
   .get(getOneEmployee)
   .delete(deleteEmployee);
-  router.route("/restore/:id").patch(restoreEmployee);
-  router.get("/deleted", getDeletedEmployees);
+router.route("/restore/:id").patch(restoreEmployee);
+router.get("/deleted", getDeletedEmployees);
+
+
 
 export default router;
