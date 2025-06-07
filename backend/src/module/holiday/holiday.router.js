3e76c0e1f57107c0d107e7384f51def78f2302ba
Adel-elbamb <engadel987654321@gmail.com>
2025-06-07 11:17:56 +0300

    feat(holiday)add validation file using joi for Create and update and delete and edit holiday.router file to support this validation

--- backend/src/module/holiday/holiday.router.js
+++ backend/src/module/holiday/holiday.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { createHoliday ,updateHoliday , deleteHoliday , allHoliday } from "./controller/holiday.controller.js";
-
-
+import { createHolidaySchema , updateHolidaySchema , deleteHolidaySchema } from "./holiday.validation.js";
+import validation from './../../middleware/validation.js'
 const router = Router();
 
-router.post("/",createHoliday);
-router.put('/:id',updateHoliday)
-router.delete('/:id', deleteHoliday)
+router.post("/",validation(createHolidaySchema),createHoliday);
+router.put('/:id',validation(updateHolidaySchema),updateHoliday)
+router.delete('/:id',validation(deleteHolidaySchema), deleteHoliday)
 router.get('/' , allHoliday)
 export default router;
