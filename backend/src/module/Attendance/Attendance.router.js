1c6387ef12d39a75a2d07de7e12af985b55dfe72
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-06-11 04:06:04 +0300

    feat(attendance): add getAttendanceByid

--- backend/src/module/Attendance/Attendance.router.js
+++ backend/src/module/Attendance/Attendance.router.js
@@ -3,10 +3,14 @@ import  Router  from "express";
 const router = Router();
 import {addAttendance} from './Controller/AddAttendance.controller.js'
 import {getAllAttendance} from './Controller/GetAllAttendance.controller.js'
+import {getAttendanceById} from './Controller/GetAttendanceById.controller.js'
 router.route('/')
       .get(getAllAttendance)
       .post(addAttendance);
 
+router.route('/:id').get(getAttendanceById)
+    //   .put(updateAttendance)
+    //   .delete(deleteAttendance);
 
 
 
