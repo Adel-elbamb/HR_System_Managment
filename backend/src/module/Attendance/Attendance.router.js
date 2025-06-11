c6c70a999b39630b2eaa6f1074b95a779c219d93
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-06-12 01:15:38 +0300

    feat(attendance): add validation

--- backend/src/module/Attendance/Attendance.router.js
+++ backend/src/module/Attendance/Attendance.router.js
@@ -6,9 +6,12 @@ import {getAllAttendance} from './Controller/GetAllAttendance.controller.js'
 import {getAttendanceById} from './Controller/GetAttendanceById.controller.js'
 import {updateAttendance} from './Controller/UpdateAttendance.controller.js'
 import {deleteAttendance} from './Controller/DeleteAttendance.controller.js'
+import validation from '../../middleware/validation.js'
+import { addAttendanceSchema } from "./Attendance.validation.js";
+
 router.route('/')
       .get(getAllAttendance)
-      .post(addAttendance);
+      .post(validation(addAttendanceSchema),addAttendance);
 
 router.route('/:id').get(getAttendanceById)
       .put(updateAttendance)
