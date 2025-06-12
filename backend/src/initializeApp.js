38288eed4be4a17e3da3a35b5584b8af496a8f93
YousefGamgoum <yousefsalah959@gmail.com>
2025-06-12 23:03:30 +0300

    rename routes

--- backend/src/initializeApp.js
+++ backend/src/initializeApp.js
@@ -4,30 +4,25 @@ import authRoutes from "./module/Auth/auth.routes.js";
 import cors from "cors";
 import path from "path";
 import PayRoll from "./module/PayRoll/payroll.routes.js";
-import AttendanceRouter from './module/Attendance/Attendance.router.js';
+import AttendanceRouter from "./module/Attendance/Attendance.router.js";
 import holidayRouter from "./module/holiday/holiday.routes.js";
 import departmentRouter from "./module/Department/Department.routes.js";
 import EmployeeRouter from "./module/Employee/Employee.routes.js";
 import { auth } from "./middleware/auth.js";
-
 
 const initializeApp = (app, express) => {
   app.use(express.json());
   app.use(cors());
   connection();
 
-
-
-
   app.use("/api/auth", authRoutes);
   app.use(auth);
-
-  app.use('/attendance',AttendanceRouter);
+  app.use("/attendance", AttendanceRouter);
   app.use("/api/holiday", holidayRouter);
   app.use("/api/employee", EmployeeRouter);
   // Register routes
   app.use("/api/department", departmentRouter);
-  app.use('/api/payroll',PayRoll);
+  app.use("/api/payroll", PayRoll);
 
   app.use(globalError);
   app.use("/{*any}", (req, res, next) => {
