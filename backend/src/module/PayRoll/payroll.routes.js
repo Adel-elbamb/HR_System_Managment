a71a6d5c952186afe980330aed6f04375a08c0c4
Ahmedhamad99 <ahaahmf123456789@gmail.com>
2025-06-12 04:10:49 +0300

    feat(payroll): add validations for add and update

--- backend/src/module/PayRoll/payroll.routes.js
+++ backend/src/module/PayRoll/payroll.routes.js
@@ -5,13 +5,18 @@ import {getAllPayroll} from './controller/GetAllPayRolls.controller.js';
 import {getPayRollById} from './controller/GetPayRollById.controller.js';
 import {updatePayRoll} from './controller/UpdatePayRoll.controller.js';
 import {deletePayroll} from './controller/DeletePayRoll.controller.js';
+
+import validation from '../../middleware/validation.js';
+
+import {addPayrollSchema,updatePayrollSchema} from  './payroll.validation.js'
+
 router.route('/')
-      .post(addPayRoll)
+      .post(validation(addPayrollSchema),addPayRoll)
       .get(getAllPayroll)
 
 router.route('/:id')
        .get(getPayRollById)
-       .put(updatePayRoll)
+       .put(validation(updatePayrollSchema),updatePayRoll)
        .delete(deletePayroll)
                 
 
