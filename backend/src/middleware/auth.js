2b30e89321cf40a73909f6f361e38f33ee9189f6
YousefGamgoum <yousefsalah959@gmail.com>
2025-06-18 16:29:31 +0300

    Refactor(Auth): streamline token extraction and validation logic in auth middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,50 +1,10 @@
-
-import jwt from 'jsonwebtoken';
-import { asyncHandler } from '../utils/asyncHandler.js';
-import { tokenBlacklist } from '../module/Auth/blacklist.js'; 
-
-
-// export const auth = asyncHandler((req, res, next) => {
-//   const authorization = req.headers['authorization'];
-//   if (!authorization || !authorization.startsWith(process.env.BEARERTOKEN)) {
-//     return next(new Error("Please login, you are not authorized"));
-//   }
-
-//   const token = authorization.split(" ")[1];
-//   console.log(token)
-//   try {
-//     const decoded = jwt.verify(token,process.env.SIGNTURE)
-//     req.user = decoded;
-//     console.log(decoded)
-//     next();
-//   } catch (err) {
-//     return next(new Error("Invalid token"));
-//   }
-// });
-
-
-
-
-// export const auth = asyncHandler((req, res, next) => {
-//   const token = req.headers['authorization'];
-
-//   if (!token) {
-//     return next(new Error("Please login, you are not authorized"));
-//   }
-//   try {
-//     const decoded = jwt.verify(token, process.env.SIGNTURE);
-//     req.user = decoded;
-//     console.log(decoded)
-//     next();
-//   } catch (err) {
-//     return next(new Error("Invalid token"));
-//   }
-// });
-
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { tokenBlacklist } from "../module/Auth/blacklist.js";
 
 export const auth = asyncHandler((req, res, next) => {
-  const token = req.headers['authorization'];
-  // const token = raw && raw.startsWith('Bearer ') ? raw.split(' ')[1] : null;
+  const raw = req.headers["authorization"];
+  const token = raw && raw.startsWith("Bearer ") ? raw.split(" ")[1] : null;
 
   if (!token) {
     return next(new Error("Please login, you are not authorized"));
