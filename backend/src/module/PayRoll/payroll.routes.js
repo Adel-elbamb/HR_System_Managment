e894aac4b3f134b3b96f9b3aaef45994aa35714d
YousefGamgoum <yousefsalah959@gmail.com>
2025-06-25 04:04:31 +0300

    Refactor(Payroll): clean up routes by removing unused endpoints and improving import statements

--- backend/src/module/PayRoll/payroll.routes.js
+++ backend/src/module/PayRoll/payroll.routes.js
@@ -1,24 +1,10 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import {addPayRoll} from './controller/AddPayRoll.controller.js';
-import {getAllPayroll} from './controller/GetAllPayRolls.controller.js';
-import {getPayRollById} from './controller/GetPayRollById.controller.js';
-import {updatePayRoll} from './controller/UpdatePayRoll.controller.js';
-import {deletePayroll} from './controller/DeletePayRoll.controller.js';
+import { getAllPayroll } from "./controller/GetAllPayRolls.controller.js";
+import { getPayRollById } from "./controller/GetPayRollById.controller.js";
 
-import validation from '../../middleware/validation.js';
+router.route("/").get(getAllPayroll);
 
-import {addPayrollSchema,updatePayrollSchema} from  './payroll.validation.js'
+router.route("/:id").get(getPayRollById);
 
-router.route('/')
-      .post(validation(addPayrollSchema),addPayRoll)
-      .get(getAllPayroll)
-
-router.route('/:id')
-       .get(getPayRollById)
-       .put(validation(updatePayrollSchema),updatePayRoll)
-       .delete(deletePayroll)
-                
-
-
-export  default router;
+export default router;
